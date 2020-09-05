2de8476bf705da47bb1a7454c635bbc7276ffeae
Karl Beck <karljohnbeck@gmail.com>
2020-09-04 23:42:05 -0500

    aded API for edamam and working with card displays

--- server/routes/userInfo.router.js
+++ server/routes/userInfo.router.js
@@ -1,19 +1,19 @@
 const express = require('express');
 const pool = require('../modules/pool');
 const router = express.Router();
+require('dotenv').config();
+axios = require('axios')
 const {
   rejectUnauthenticated,
 } = require('../modules/authentication-middleware');
 
 router.get('/', rejectUnauthenticated, (req, res) => {
-  console.log('req.user:', req.user);
-  pool
-    .query(`SELECT * FROM "user";`,)
-    .then((results) => res.send(results.rows))
-    .catch((error) => {
-      console.log('Error making SELECT for secrets:', error);
-      res.sendStatus(500);
-    });
+  // axios.get(`https://api.edamam.com/search?q=chicken&app_id=${process.env.APP_ID}&app_key=${process.env.EDAMAM_KEY}`)
+  //   .then((results) => res.send(results.data))
+  //   .catch((error) => {
+  //     console.log('Error making SELECT for secrets:', error);
+  //     res.sendStatus(500);
+  //   });
 });
 
 module.exports = router;
--- src/components/UserInfo/UserInfo.js
+++ src/components/UserInfo/UserInfo.js
@@ -2,6 +2,40 @@ import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import mapStoreToProps from '../../redux/mapStoreToProps';
 
+import Card from '@material-ui/core/Card';
+import CardActions from '@material-ui/core/CardActions';
+import CardContent from '@material-ui/core/CardContent';
+import Button from '@material-ui/core/Button';
+import Typography from '@material-ui/core/Typography';
+import { withStyles, Grid } from '@material-ui/core';
+
+const styles = {
+  gridContainer: {
+    paddingLeft: '10px',
+    paddingRight: '10px',
+  },
+  card: {
+  },
+  bullet: {
+    display: 'inline-block',
+    margin: '0 2px',
+    transform: 'scale(0.8)',
+  },
+  title: {
+    fontSize: 14,
+  },
+  pos: {
+    marginBottom: 12,
+  },
+};
+
+const stylez = theme => ({
+  root: {
+    backgroundColor: "red"
+  }
+});
+
+
 // Basic class component structure for React with default state
 // value setup. When making a new component be sure to replace
 // the component name TemplateClass with the name for the new
@@ -9,20 +43,71 @@ import mapStoreToProps from '../../redux/mapStoreToProps';
 class UserInfo extends Component {
   state = {
     heading: 'UserInfo Component',
+  
   };
 
+  
+
+
   componentDidMount() {
-      this.props.dispatch({type: 'FETCH_USERINFO'})
+    this.props.dispatch({ type: 'FETCH_USERINFO' })
   }
 
   render() {
+    const { classes } = this.props;
     return (
-      <div>
-        <h2>{this.state.heading}</h2>
-        {JSON.stringify(this.props.store.userInfoReducer)}
-      </div>
+      <Grid container spacing={2} className={classes.gridContainer} >
+        <Grid item xs={6}>
+      <Card className={classes.card}>
+      <CardContent>
+        <Typography className={classes.title} color="textSecondary" gutterBottom>
+          Word of the Day
+        </Typography>
+        <Typography variant="h5" component="h2">
+          
+        </Typography>
+        <Typography className={classes.pos} color="textSecondary">
+          adjective
+        </Typography>
+        <Typography component="p">
+          well meaning and kindly.
+          <br />
+          {'"a benevolent smile"'}
+        </Typography>
+      </CardContent>
+      <CardActions>
+        <Button size="small">Learn More</Button>
+      </CardActions>
+    </Card>
+    </Grid>
+    <Grid item xs={6} >
+      <Card className={classes.card}>
+      <CardContent>
+        <Typography className={classes.title} color="textSecondary" gutterBottom>
+          Word of the Day
+        </Typography>
+        <Typography variant="h5" component="h2">
+          
+        </Typography>
+        <Typography className={classes.pos} color="textSecondary">
+          adjective
+        </Typography>
+        <Typography component="p">
+          well meaning and kindly.
+          <br />
+          {'"a benevolent smile"'}
+        </Typography>
+      </CardContent>
+      <CardActions>
+        <Button size="small">Learn More</Button>
+      </CardActions>
+    </Card>
+    </Grid>
+    </Grid>
     );
   }
 }
 
-export default connect(mapStoreToProps)(UserInfo);
+const styleUserInfo = withStyles(styles)(UserInfo)
+
+export default connect(mapStoreToProps)(styleUserInfo);
